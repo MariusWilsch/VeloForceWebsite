2934905e941644af1c60cd415663c2b8c58e73c8
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2024-03-14 13:30:52 +0100

    Added header to array in forWho

--- my-app/app/forWho/forWho.tsx
+++ my-app/app/forWho/forWho.tsx
@@ -24,16 +24,45 @@ const SideBySideContentBox = () => {
     "Business developers looking for operational insights.\nOur softwares allow for extraction of the specific data you need  in order to give insights into the most valuable operations for the business, which allows for systematic growth over time."
   ];
 
+  // Create an array of objets that has the headers and the text as values
+  const contentArray = [
+    {
+      header: "Header 1",
+      text: "Our services streamline operations and enhance productivity by taking away all repetitive tasks, allowing you to focus on strategic growth."
+    },
+    {
+      header: "Header 2",
+      text: "Competitive Startups Aiming to Outshine compitition Veloxforce makes it possible to provide you with custom tools tailored to your needs to find untapped potential and get ahead of your competition."
+    },
+    {
+      header: "Header 3",
+      text: "Established companies with outdated operations Upgrade your well-established business with software that simplifies daily tasks. Letting your administration, payments and customer service run on autopilot so your team can focus on the most valuable tasks."
+    },
+    {
+      header: "Header 4",
+      text: "Overworked Teams Seeking a Breather\nOur automations are a lifeline for businesses drowning in day-to-day tasks. For the business owner who spends more time managing emails and spreadsheets than growing their business."
+    },
+    {
+      header: "Header 5",
+      text: "Businesses facing high frequency processing\nOur solutions make sure the high throughput of you operation will relieve you from your manual interventions. from the moment of receiving data to filing tax papers."
+    },
+    {
+      header: "Header 6",
+      text: "Business developers looking for operational insights.\nOur softwares allow for extraction of the specific data you need  in order to give insights into the most valuable operations for the business, which allows for systematic growth over time."
+    }
+  ];
+
+
   return (
     <div className='p-section'>
       <Content topper="Why Choose 8?" title="We're the best in the business" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed aliquam dictum sapien, id sagittis augue malesuada eu." />
-      {content.map((text, index) => (
+      {contentArray.map((item, index) => (
         <div className="flex text-white m-10" key={index}>
           {index % 2 === 0 ? (
             <>
               <div className="w-1/2 p-4">
-                <h1 className="text-xl font-bold mb-4">Small to mid-sized businesses  aiming to build a foundation to scale upon</h1>
-                <p>{text}</p>
+                <h1 className="text-xl font-bold mb-4">{ item.header }</h1>
+                <p>{item.text}</p>
               </div>
               <div className="w-1/2 p-4"></div>
             </>
@@ -41,8 +70,8 @@ const SideBySideContentBox = () => {
             <>
               <div className="w-1/2 p-4"></div>
               <div className="w-1/2 p-4">
-                <h1 className="text-xl font-bold mb-4">Competitive Startups Aiming to Outshine compitition </h1>
-                <p>{text}</p>
+                  <h1 className="text-xl font-bold mb-4"> {item.header} </h1>
+                  <p>{item.text}</p>
               </div>
             </>
           )}
