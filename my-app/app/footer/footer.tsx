8c6284a2a9a7c4a8875a666ae1c748456a6d9511
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2024-03-14 12:05:50 +0100

    Removed hover color on team and remove top section of footer

--- my-app/app/footer/footer.tsx
+++ my-app/app/footer/footer.tsx
@@ -1,33 +1,12 @@
 import './footer.css';
 
-// Create a list item component that I can reuse in Top Section
-
-interface ListItemProps {
-  link: string;
-  text: string;
-}
-
-const ListItem: React.FC<ListItemProps> = ({ link, text }) => {
+const SocialLink = ({ label, url, iconSrc }) => {
   return (
-    <li className="cs-li underline-on-hover">
-      <a href={link} className="cs-link ">{text}</a>
-    </li>
-  );
-}
-
-const TopSection = () => {
-  return (
-    <div className="cs-top">
-      <a aria-label="go back to home" className="cs-logo" href="">
-        <img className="cs-logo-img" loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Logos/agency-logo.svg" alt="logo" width="209" height="40" />
+    <li className="cs-social-li">
+      <a href={url} className="cs-social-link" aria-label={label} target="_blank" rel="noopener">
+        <img className="cs-social-icon cs-default" src={iconSrc} alt="icon" loading="lazy" decoding="async" width="12" height="12" aria-hidden="true" />
       </a>
-      <ul className="cs-ul">
-        <ListItem link="" text="Home" />
-        <ListItem link="" text="About" />
-        <ListItem link="" text="Services" />
-        <ListItem link="" text="Contact" />
-      </ul>
-    </div>
+    </li>
   );
 };
 
@@ -35,26 +14,10 @@ const BottomSection = () => {
   return (
     <div className="cs-bottom">
       <ul className="cs-social">
-        <li className="cs-social-li">
-          <a href="" className="cs-social-link" aria-label="facebook" target="_blank" rel="noopener">
-            <img className="cs-social-icon cs-default" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/facebook-ba.svg" alt="icon" loading="lazy" decoding="async" width="12" height="12" aria-hidden="true" />
-          </a>
-        </li>
-        <li className="cs-social-li">
-          <a href="" className="cs-social-link" aria-label="twitter" target="_blank" rel="noopener">
-            <img className="cs-social-icon cs-default" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/twitter-ba.svg" alt="icon" loading="lazy" decoding="async" width="12" height="12" aria-hidden="true" />
-          </a>
-        </li>
-        <li className="cs-social-li">
-          <a href="" className="cs-social-link" aria-label="instagram" target="_blank" rel="noopener">
-            <img className="cs-social-icon cs-default" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/instagram-ba.svg" alt="icon" loading="lazy" decoding="async" width="12" height="12" aria-hidden="true" />
-          </a>
-        </li>
-        <li className="cs-social-li">
-          <a href="" className="cs-social-link" aria-label="twitter" target="_blank" rel="noopener">
-            <img className="cs-social-icon cs-default" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/youtube-ba.svg" alt="icon" loading="lazy" decoding="async" width="12" height="12" aria-hidden="true" />
-          </a>
-        </li>
+        <SocialLink label="facebook" url="" iconSrc="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/facebook-ba.svg" />
+        <SocialLink label="twitter" url="" iconSrc="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/twitter-ba.svg" />
+        <SocialLink label="instagram" url="" iconSrc="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/instagram-ba.svg" />
+        <SocialLink label="youtube" url="" iconSrc="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/youtube-ba.svg" />
       </ul>
       <span className="cs-copyright">
         © Copyright 2023 - <a href="" className="cs-copyright-link">Veloxforce</a>
@@ -71,8 +34,10 @@ const BottomSection = () => {
 const Footer = () => {
   return (
     <section id="footer-1390">
-      <div className="cs-container">
-        <TopSection />
+
+      <div className="cs-container ">
+        <div className="cs-top">
+        </div>
         <BottomSection />
       </div>
     </section>
