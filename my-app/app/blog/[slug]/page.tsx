dc6d19c888b0140a9e2d644ca0311a2ee751104e
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2024-03-15 08:16:28 +0100

    Check responsive in blog routes

--- my-app/app/blog/[slug]/page.tsx
+++ my-app/app/blog/[slug]/page.tsx
@@ -29,7 +29,7 @@ const BlogArticle = async ({ params }: { params: { slug: string } }) => {
     <div className='w-full  flex flex-col items-center text-center p-section'>
       <SectionHeader topper='Blog' title={data.title} text='' />
       <Image src={urlFor(data.titleImage).url()} alt={data.title} width={600} height={400} priority className='mt-8 rounded-lg' />
-      <div className='mt-16 w-3/5 prose-stone prose-xl text-white text-left prose-li:text-secondary prose-headings:font-bold prose-p:text-gray-200 '>
+      <div className='mt-16 w-full md:w-3/5 prose-stone prose-lg md:prose-xl 2xl:prose-2xl text-white text-left prose-li:text-secondary  prose-headings:font-bold prose-p:text-gray-200 '>
         <PortableText value={data.content} />
       </div>
     </div>
