45806de1db47b20ea5e55017b4d346bc5a0e5893
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2024-03-15 16:32:48 +0100

    Increase size of logo

--- my-app/app/header/header.tsx
+++ my-app/app/header/header.tsx
@@ -15,7 +15,7 @@ const Logo = () => (
         src={LogoImage}
         width={143}
         height={128}
-        className='w-auto h-8 sm:h-12'
+        className='w-auto h-10 sm:h-14'
       />
     </Link>
   </>
